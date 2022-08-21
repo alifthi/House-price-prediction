de6a3871d21d9f8caa6cc8e55da82b79c26e30cf
ALi fathi <alifathi8008@gmail.com>
2022-08-21 23:32:53 +0430

    main script

--- main.py
+++
@@ -1,8 +0,0 @@
-from HousePricePrediction import  Model
-
-model = Model()
-txtPath = r'/home/alifathi/Documents/AI/Git/House price prediction/Data/HousesInfo.txt'
-imgPath = r'/home/alifathi/Documents/AI/Git/House price prediction/Data/house_dataset'
-model.loadData(txtPath=txtPath,imgPath=imgPath)
-model.compile(loss = 'mse',metrics = ['mse'],optim = tf.keras.optimizers.Adam(learning_rate = 0.01))
-model.train(1,512)
